f7866b5097b954504ba268e1d0350cb8fbaff927
Kevin Tailor <kevin7aylor@gmail.com>
2021-05-31 20:35:09 +0530

    SK

--- course/views.py
+++ course/views.py
@@ -1,6 +1,7 @@
 from django.shortcuts import render
 from django.http import HttpResponse
 # Create your views here.
+# SECRET_KEY = 'django-insecure-*5#v+#!^(y8tcq3)$y@-p98u)0500z^0m+u(f04fcbb5t))+(='
 def my_fun(request):
     return HttpResponse('<h1>Hello course insie url course</h1>')
 

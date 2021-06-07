d8c077197bd8014cce6bd4ceccaf2b8c36e3b79e
Kevin Tailor <kevin7aylor@gmail.com>
2021-06-07 21:55:24 +0530

    Creating dynamic template

--- course/urls.py
+++ course/urls.py
@@ -1,6 +1,8 @@
 from django.urls import path
 from . import views
 
+# INDEPENDENT URL PATTERNS
 urlpatterns = [
     path('course/', views.my_fun),
+    path('dynamic/',views.dashboard)
 ]
--- course/views.py
+++ course/views.py
@@ -1,9 +1,17 @@
 from django.shortcuts import render
 from django.http import HttpResponse
 # Create your views here.
-# SECRET_KEY = 'django-insecure-*5#v+#!^(y8tcq3)$y@-p98u)0500z^0m+u(f04fcbb5t))+(='
+
 def my_fun(request):
+    # RETURNING HTTP RESPONSE
     return HttpResponse('<h1>Hello course insie url course</h1>')
 
 def home(request):
+    # SIMPLE DEMO OF RENDERING HTML TEMPLATE
     return render(request,'home.html')
+
+def dashboard(request):
+    # DICTIONARy
+    courseName = {'name': 'Github'}
+    # PASS IT TO RENDER FUNCTION
+    return render(request,'dashboard.html',courseName)

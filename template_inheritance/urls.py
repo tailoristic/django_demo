9f051ee558781f35e92e5359588f86ece9f40433
Kevin Tailor <kevin7aylor@gmail.com>
2021-07-13 21:35:45 +0530

    Include tag

--- template_inheritance/urls.py
+++ template_inheritance/urls.py
@@ -2,6 +2,6 @@ from django.urls import path
 from . import views
 urlpatterns = [
     path('base/', views.master_index),
-    path('home_page/', views.home_page),
-    path('about-page/', views.about_page),
+    path('home_page/', views.home_page, name="homePage"),
+    path('about-page/', views.about_page, name="aboutPage"),
 ]
